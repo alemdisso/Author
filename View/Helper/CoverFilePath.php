25d0185ab836dab486e696b90663a9fcc4eee013
alemdisso <rodrigo.machado@alemdisso.com.br>
2016-02-15 17:31:22 -0200

    status cover resize

--- View/Helper/CoverFilePath.php
+++ View/Helper/CoverFilePath.php
@@ -2,15 +2,27 @@
 
 class Author_View_Helper_CoverFilePath extends Zend_View_Helper_Abstract
 {
-    public function coverFilePath(Author_Collection_Edition $editionObj, $noImgFilename="no_img.png")
+    public function coverFilePath(Author_Collection_Edition $editionObj, $noImgFilename="no_img.png", $type="md")
     {
         $filename = $editionObj->getCover();
         if ($filename == "") {
             $coverFilePath = "/img/$noImgFilename";
         } else {
-            $coverFilePath = '/img/editions/raw/' . $filename;
+            switch ($type) {
+                case "tb":
+                case "md":
+                case "raw" :
+                    $folder = $type;
+                    break;
+                
+                default:
+                    $folder = "md";
+                    break;
+            }
         }
-
+            
+        $coverFilePath = '/img/editions/' . $folder . '/' . $filename;
+ 
         return $coverFilePath;
     }
 }
